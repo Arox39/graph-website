bf27dbd484b6d8019188bd115f81582ee6bd03e4
Arox39 <amaury.morot39@gmail.com>
2023-08-20 10:37:05 +0200

    fix je prend contact btn

--- src/pages/home/components/header/header.component.jsx
+++ src/pages/home/components/header/header.component.jsx
@@ -8,7 +8,7 @@ const Header = () => {
             <h1 className='title'> L'agence de <span className='underline'>communication</span> & <span className='blue'> de marketing digital.</span></h1>
             <h3 className='homeCitation'>"Cultivons ensemble votre image"</h3>
             <p className='description'>GraphiLeaf cultive l'image de marque de ses clients et aide les entreprises à raconter leur histoire unique grâce à une stratégie de communication efficace et des designs visuels de qualité.</p>
-            <Btn title="Je prend contact" color="black" width="225px" padding="0 0 0 20px" height="40px"/>
+            <Btn title="Je prend contact" color="black" width="225px" padding="0 0 0 20px" height="41px" />
             <img className='headerImage' src={headerImage} alt='phone illustration'/>
 
         </header>
