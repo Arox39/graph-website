b45346c992fe3b4493f8a26252793a45a4fbc7e9
Arox39 <amaury.morot39@gmail.com>
2023-08-24 10:23:20 +0200

    finish the agence page

--- src/pages/agence/agence.component.jsx
+++ src/pages/agence/agence.component.jsx
@@ -2,6 +2,7 @@ import './agence.style.scss'
 
 import illustration from '../../assets/img/Nuage.png'
 import bluegrid from '../../assets/img/BG-PB.svg'
+import Solution from './components/solution/solution.component'
 import Footer from '../../components/footer/footer.component'
 const Agence = () => {
     return (
@@ -17,10 +18,36 @@ const Agence = () => {
             <div className='team'>
                 <h1 className='title'>Une équipe <span className='underline'>complète</span> <br/>
                 & <span className='blue'>compétente.</span></h1>
+
                 <p className='team-content'>
                 Spécialisés dans la <span className='blue'>communication</span> et le <span className='blue'>marketing digital</span>, nous avons développé une expertise dans plusieurs domaines clés pour aider les petites entreprises, les structures et les indépendants à maximiser leur présence et leur développement en ligne. <br/> <br/> Notre équipe <span className='blue'>créative et dynamique</span> se compose d'experts à chaque domaine. Des compétences diverses et une approche unique et personnalisée, afin d'élaborer des stratégies sur mesure pour répondre à <span className='blue'>vos besoins.</span></p>
+
                 <img className='blue-grid' src={bluegrid} alt='a blue grid'/>
             </div>
+
+            <Solution/>
+
+            <div className='why'>
+
+                <h1 className='title'>Pourquoi <span className='blue'>GraphiLeaf</span> ?</h1>
+                <div className='why-content'>
+                    <p className='left'>
+                        Nous sommes <strong>passionnés par chaque projet</strong> que nous entreprenons. Nous nous engageons à fournir des résultats de qualité supérieure, en accord avec <strong>vos besoins et vos objectifs</strong>. Nous croyons fermement que chaque entreprise a <strong>son histoire</strong> et mérite une stratégie de communication unique pour la partager avec le monde. C'est pourquoi nous travaillons en étroite collaboration avec nos clients pour comprendre leur vision et élaborer des <strong>solutions personnalisées</strong>.
+                    </p>
+                    
+                    <div className='right'>
+                    <strong>GraphiLeaf ?</strong> C'est simple, 
+                    <ul>
+                        <li><strong>Graphi</strong> <span> = La conception graphique et la création d’images numériques</span></li>
+
+                        <li><strong>Leaf</strong> <span> = a croissance, le renouveau et la nature. Reflète la mission d’aider les entreprises à se développer et atteindre leurs objectifs marketing tout en respectant l’environnement</span> </li>
+                    </ul>
+                    </div>
+                </div>
+            </div>
+
+
+
 
             <Footer />
         </div>
--- src/pages/agence/components/solution/solution.component.jsx
+++ src/pages/agence/components/solution/solution.component.jsx
@@ -1,8 +1,46 @@
+import { Link } from 'react-router-dom'
+import ArrowEffect from '../../../../components/shared/arrowEffect/arrowEffect.component'
 import './solution.style.scss'
-
+import greygrid from '../../../../assets/img/BG-PG.svg'
 const Solution = () => {
     return(
-        <div></div>
+        <div className='solution-container'>
+            <h2 className='solution-title'>Des solutions sur mesure pour chaque besoins :</h2>
+
+            <ul className='solution-list'>
+                <li className='solution-element'>
+                    <Link to={''} className='btn'>
+                        Graphisme <span>(identité visuelle, support publicitaire, social media design...)</span>
+                        <ArrowEffect arrowColor='blue'/>
+                    </Link>
+                </li>
+                <li className='solution-element'> 
+                    <Link to={''} className='btn'>
+                        Vidéo <span> (TikTok, YouTube, Reels...)</span>
+                        <ArrowEffect arrowColor='blue'/>
+                    </Link>
+                </li>
+                <li className='solution-element'> 
+                    <Link to={''} className='btn'>
+                        Social management  <span>(gestion des réseaux sociaux, création de contenus, analyse des concurrents...)</span>
+                        <ArrowEffect arrowColor='blue'/>
+                    </Link>
+                </li>
+                <li className='solution-element'>
+                    <Link to={''}  className='btn'>
+                        Veille opérationnelle  <span>(surveillance et analyse de l'image de marque, ajustement des stratégies)</span>
+                        <ArrowEffect arrowColor='blue'/>
+                    </Link>
+                </li>
+                
+            </ul>
+            <img className='grey-grid' src={greygrid} alt='grey grid'/>
+            <Link to={''} className='solution-btn btn'>
+                Échangeons sur votre projet 
+                <ArrowEffect bgcolor='#005570' />
+            </Link>
+
+        </div>
     )
 }
 
